dfaed32f3e20fe1392c2e88b7da005b10ebca9b6
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2017-11-30 08:31:15 +1100

    Add travis trigger to build stackstorm prebake image

---
+++ travis-remote-trigger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_ACCESS_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/umccr%2Fst2-docker-pack-prebuild-umccr/requests
