2ae8043fd9825d00755d2e35615c7699a47e5416
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-01 13:23:38 +1100

    Attempt to detect 10X sample sheet and fail until supported

--- etc/runfolder-samplesheet-check.py
+++ etc/runfolder-samplesheet-check.py
@@ -22,6 +22,8 @@ def write_log(msg):
     print(msg, file=LOG_FILE)
 
 # TODO: validate input parameter?
+# TODO: still need to handle 10X cases....
+
 samplesheet_file_path = sys.argv[1]
 
 samplesheet_name = os.path.basename(samplesheet_file_path)
@@ -33,6 +35,10 @@ write_log("INFO: Checking SampleSheet %s" % samplesheet_file_path)
 # first create all index length tuples
 sorted_samples = collections.defaultdict(list)
 for sample in sample_sheet:
+    # simple 10X check, has to be improved!
+    if sample.Sample_ID.startswith("SI-GA"):
+        write_log("ERROR: Looks like a 10X dataset. Cannot handle this yet.")
+        exit(1)
     index_length = len(sample.index.replace("N",""))
 
     if sample.index2:
@@ -45,12 +51,11 @@ for sample in sample_sheet:
 
 
 
-samplesheet_files = list()
 if len(sorted_samples) is 1:
     key, value = sorted_samples.popitem()
     write_log("INFO: Only one index lengths combination (%s\%s). No need for custom sample sheets." % ( key[0], key[1] ))
 else:
-    write_log("INFO: Multiple index lengths combination. Need custom sample sheets.")
+    write_log("INFO: Multiple index lengths combination. Creating custom sample sheets.")
     count=0
     for key in sorted_samples:
         count += 1
@@ -63,14 +68,13 @@ else:
         for sample in sorted_samples[key]:
           new_sample_sheet.add_sample(sample)
 
-        new_sample_sheet_file = os.path.join(samplesheet_dir, samplesheet_name + ".custom" + count)
-        samplesheet_files.append(new_sample_sheet_file)
+        new_sample_sheet_file = os.path.join(samplesheet_dir, samplesheet_name + ".custom" + str(count))
+        write_log("INFO: Creating custom sample sheet: %s" % new_sample_sheet_file)
         f = open(new_sample_sheet_file, "w")
         new_sample_sheet.write(f)
         f.close()
+        write_log("DEBUG: Created custom sample sheet: %s" % new_sample_sheet_file)
 
-
-write_log("INFO: Created files: %s" % samplesheet_files)
 
 LOG_FILE.close()
 SOCK.close()
