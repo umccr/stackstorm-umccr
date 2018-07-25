1924ae3d044ca1d6eb5db1063ebc5482c5754c95
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-07-25 15:57:29 +1000

    Make script deploy env aware

--- etc/establish-ssh-tunnels.sh
+++ etc/establish-ssh-tunnels.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
-# create a tunnel for the arteria runfolder service on port 8888
-autossh -M 30000 -f -N -R 8888:0.0.0.0:8888 ubuntu@stackstorm.prod.umccr.org &
+if test -z "$DEPLOY_ENV"; then
+    echo "DEPLOY_ENV is not set! Set it to either 'dev' or 'prod'."
+    exit 1
+fi
 
-# create a tunnel for general ssh access (novastor ssh is configured for port 4321)
-autossh -M 31000 -f -N -R 2222:0.0.0.0:4321 ubuntu@stackstorm.prod.umccr.org &
+# create a tunnel for the arteria runfolder service on port 8888 and
+# for general ssh access (novastor ssh is configured for port 4321)
+if test "$DEPLOY_ENV" = "prod"; then
+    autossh -M 30000 -f -N -R 8888:0.0.0.0:8888 ubuntu@stackstorm.prod.umccr.org &
+    autossh -M 31000 -f -N -R 2222:0.0.0.0:4321 ubuntu@stackstorm.prod.umccr.org &
+else
+    autossh -M 40000 -f -N -R 8888:0.0.0.0:8888 ubuntu@stackstorm.dev.umccr.org &
+    autossh -M 41000 -f -N -R 2222:0.0.0.0:4321 ubuntu@stackstorm.dev.umccr.org &
+fi
