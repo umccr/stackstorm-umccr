d39fc7733fc0647213920a628ad135585d33eb3a
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-08-10 14:21:25 +1000

    Fix ssh port for dev

--- etc/establish-ssh-tunnels.sh
+++ etc/establish-ssh-tunnels.sh
@@ -12,6 +12,6 @@ if test "$DEPLOY_ENV" = "prod"; then
     autossh -M 30000 -f -N -R 8888:0.0.0.0:8888 ubuntu@stackstorm.prod.umccr.org &
     autossh -M 31000 -f -N -R 2222:0.0.0.0:4321 ubuntu@stackstorm.prod.umccr.org &
 else
-    autossh -M 40000 -f -N -R 8888:0.0.0.0:8888 ubuntu@stackstorm.dev.umccr.org &
+    autossh -M 40000 -f -N -R 8888:0.0.0.0:8899 ubuntu@stackstorm.dev.umccr.org &
     autossh -M 41000 -f -N -R 2222:0.0.0.0:4321 ubuntu@stackstorm.dev.umccr.org &
 fi
